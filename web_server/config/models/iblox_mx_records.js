7740a52695993364948d666c29f0c74346ad1cc7
Mobb autofixer <git@mobb.ai>
2024-08-02 09:43:18 +0000

    mobb fix commit: 6c0e7e76-8271-4190-b8a2-e7f315b22a7a

--- web_server/config/models/iblox_mx_records.js
+++ web_server/config/models/iblox_mx_records.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -36,7 +37,7 @@ module.exports = {
     mxModel: mxModel,
     getIBMXByZonePromise: function (zone) {
         return mxModel.find({
-            'zone': zone,
+            'zone': mongoSanitize.sanitize({ data: zone }).data,
         }).exec();
     },
     getIBMXByIBloxZonePromise: function (zone) {
