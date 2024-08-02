43dd39fc2a5ab1b539cab0c6dcf7fef35fcff961
Mobb autofixer <git@mobb.ai>
2024-08-02 09:43:14 +0000

    mobb fix commit: a34e6a25-e6ab-436b-a8fc-48faacf2df99

--- web_server/config/models/ip_zone.js
+++ web_server/config/models/ip_zone.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -34,7 +35,7 @@ module.exports = {
     IpZoneModel: ipZoneModel,
     getZoneByNamePromise: function (name) {
         return ipZoneModel.findOne({
-            'zone': name,
+            'zone': mongoSanitize.sanitize({ data: name }).data,
         }).exec();
     },
     getZoneByIdPromise: function (id) {
